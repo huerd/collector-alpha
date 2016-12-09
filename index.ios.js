9fa4bb89cb12e9a01f48568cff58a5a9110e73f5
Mirren King-Smith <king-smith@MacBook-Pro.local>
2016-12-09 15:49:03 +1000

    added build for testing on iphone device

--- index.ios.js
+++ index.ios.js
@@ -3,62 +3,93 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-import { NativeModules } from 'react-native';
-import React, { Component } from 'react';
+import {NativeModules} from 'react-native';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Dimensions,
+    TouchableHighlight,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
+import Camera from 'react-native-camera';
+
+// Initialise opencv using native modules
 var opencv = NativeModules.NativeCV;
+
 opencv.cv_basicTest("test");
 
 opencv.cv_callbackTest(10, (error, output) => {
-  if(error){
-    console.log(error);
-  }else{
-    console.log(output);
-  }
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(output);
+    }
 })
 
 export default class opencv_test extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Camera ref={(cam) => {
+                    this.camera = cam;
+                }} style={styles.preview} aspect={Camera.constants.Aspect.fill}>
+                    <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+                </Camera>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.ios.js
+                </Text>
+                <Text style={styles.instructions}>
+                    Press Cmd+R to reload,{'\n'}
+                    Cmd+D or shake for dev menu
+                </Text>
+            </View>
+        );
+    }
+
+    takePicture() {
+        this.camera.capture().then((data) => console.log(data)).catch(err => console.error(err));
+    }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        color: '#000',
+        padding: 10,
+        margin: 40
+    }
 });
 
 AppRegistry.registerComponent('opencv_test', () => opencv_test);
